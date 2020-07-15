f34c2a11a5aaf9efdb10d87a40d482e5993767fa
JuanJosephG <juanjoseph50@gmail.com>
2020-07-15 10:11:53 -0500

    WWW-83: Add styles default to ImageTextSection

---
+++ src/components/molecules/ImageTextSection/StylesDefaut.js
@@ -0,0 +1,18 @@
+const StylesDefault = {
+  paper: {
+    marginBottom: 0.5,
+    padding: 2,
+    margin: "auto",
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "376px",
+  },
+  text: {
+    textAlign: "justify",
+  },
+}
+
+export default StylesDefault
