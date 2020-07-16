54eb8e84964dcc2171a6b8e95ce0faede8f50c08
JuanJosephG <juanjoseph50@gmail.com>
2020-07-16 10:51:02 -0500

    WWW-83: Fix import names

--- src/components/molecules/ImageTextSection/ImageTextSection.js
+++ src/components/molecules/ImageTextSection/ImageTextSection.js
@@ -1,5 +1,5 @@
 import React from "react"
-import defaultStyles from "./defaultStyles"
+import defaultStyles from "./DefaultStyles"
 import { makeStyles } from "@material-ui/core/styles"
 import { Grid, Paper, Typography } from "@material-ui/core"
 import { overrideStyle } from "../../../functions/stylesParser"
